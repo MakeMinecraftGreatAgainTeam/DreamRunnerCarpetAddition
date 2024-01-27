ac358bd579eb4875803846f0a1a9c7b2705de8d3
Wzp-2008 <minecraftwzpmc@163.com>
2024-01-27 12:37:41 +0800

    init project

---
+++ src/client/java/org/mmga/der/DerCarpetAdditionClient.java
@@ -0,0 +1,10 @@
+package org.mmga.der;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DerCarpetAdditionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
